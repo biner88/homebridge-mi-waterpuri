368879e8f1becb3068115920c27898d2a321b90d
biner <biner@foxmail.com>
2021-05-24 21:11:52 +0800

    1.0.1

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-# homebridge-mi-water-purifier
+# homebridge-mi-waterpuri
 
 MiWaterPurifier for homebridge
 
-Thanks for [nfarina](https://github.com/nfarina)(the author of [homebridge](https://github.com/nfarina/homebridge)), [OpenMiHome](https://github.com/OpenMiHome/mihome-binary-protocol), [aholstenson](https://github.com/aholstenson)(the author of [miio](https://github.com/aholstenson/miio)), all other developer and testers.   
+Model:yunmi.waterpuri.lx3
 
 ## TDS shows in Temperature.(No accessory available for water purifier)
 
 ## Installation
+
 1. Install HomeBridge, please follow it's [README](https://github.com/nfarina/homebridge/blob/master/README.md).   
 If you are using Raspberry Pi, please read [Running-HomeBridge-on-a-Raspberry-Pi](https://github.com/nfarina/homebridge/wiki/Running-HomeBridge-on-a-Raspberry-Pi).   
 2. Make sure you can see HomeBridge in your iOS devices, if not, please go back to step 1.   
 3. Install packages.   
 
 ```
-npm install -g miio homebridge-mi-water-purifier2
+npm install -g miio homebridge-mi-waterpuri
 ```
 
 ## Configuration
--- index.js
+++ index.js
@@ -4,85 +4,85 @@ var fs = require('fs');
 var packageFile = require("./package.json");
 var PlatformAccessory, Accessory, Service, Characteristic, UUIDGen;
 
-module.exports = function(homebridge) {
-    if(!isConfig(homebridge.user.configPath(), "platforms", "MiWaterPurifierPlatform")) {
+module.exports = function (homebridge) {
+    if (!isConfig(homebridge.user.configPath(), "platforms", "MiWaterPurifierPlatform")) {
         return;
     }
-    
+
     PlatformAccessory = homebridge.platformAccessory;
     Accessory = homebridge.hap.Accessory;
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     UUIDGen = homebridge.hap.uuid;
 
-    homebridge.registerPlatform('homebridge-mi-water-purifier', 'MiWaterPurifierPlatform', MiWaterPurifierPlatform, true);
+    homebridge.registerPlatform('homebridge-mi-waterpuri', 'MiWaterPurifierPlatform', MiWaterPurifierPlatform, true);
 }
 
 function isConfig(configFile, type, name) {
     var config = JSON.parse(fs.readFileSync(configFile));
-    if("accessories" === type) {
+    if ("accessories" === type) {
         var accessories = config.accessories;
-        for(var i in accessories) {
-            if(accessories[i]['accessory'] === name) {
+        for (var i in accessories) {
+            if (accessories[i]['accessory'] === name) {
                 return true;
             }
         }
-    } else if("platforms" === type) {
+    } else if ("platforms" === type) {
         var platforms = config.platforms;
-        for(var i in platforms) {
-            if(platforms[i]['platform'] === name) {
+        for (var i in platforms) {
+            if (platforms[i]['platform'] === name) {
                 return true;
             }
         }
     } else {
     }
-    
+
     return false;
 }
 
 function MiWaterPurifierPlatform(log, config, api) {
-    if(null == config) {
+    if (null == config) {
         return;
     }
-    
+
     this.Accessory = Accessory;
     this.PlatformAccessory = PlatformAccessory;
     this.Service = Service;
     this.Characteristic = Characteristic;
     this.UUIDGen = UUIDGen;
-    
+
     this.log = log;
     this.config = config;
 
     if (api) {
         this.api = api;
     }
-    
-    
+
+
     this.log.info("[MiWaterPurifier][INFO]*********************************************************************");
-    this.log.info("[Miwaterpurifier][INFO]                      Miwaterpurifier v%s                           *",packageFile.version);
-    this.log.info("[Miwaterpurifier][INFO] GitHub: https://github.com/Zzm317/homebridge-mi-water-purifier     *");
+    this.log.info("[Miwaterpurifier][INFO]                      Miwaterpurifier v%s                           *", packageFile.version);
+    this.log.info("[Miwaterpurifier][INFO] GitHub: https://github.com/biner88/homebridge-mi-waterpuri     *");
     this.log.info("[Miwaterpurifier][INFO]                                                                    *");
     this.log.info("[Miwaterpurifier][INFO]*********************************************************************");
     this.log.info("[Miwaterpurifier][INFO]start success...");
-    
+
 }
 
 MiWaterPurifierPlatform.prototype = {
-    accessories: function(callback) {
+    accessories: function (callback) {
         var myAccessories = [];
 
         var deviceCfgs = this.config['deviceCfgs'];
-        
-        if(deviceCfgs instanceof Array) {
+
+        if (deviceCfgs instanceof Array) {
             for (var i = 0; i < deviceCfgs.length; i++) {
                 var deviceCfg = deviceCfgs[i];
-                if(null == deviceCfg['type'] || "" == deviceCfg['type'] || null == deviceCfg['token'] || "" == deviceCfg['token'] || null == deviceCfg['ip'] || "" == deviceCfg['ip']) {
+                if (null == deviceCfg['type'] || "" == deviceCfg['type'] || null == deviceCfg['token'] || "" == deviceCfg['token'] || null == deviceCfg['ip'] || "" == deviceCfg['ip']) {
                     continue;
                 }
-                
+
                 if (deviceCfg['type'] == "MiWaterPurifier") {
-                    new MiWaterPurifier(this, deviceCfg).forEach(function(accessory, index, arr){
+                    new MiWaterPurifier(this, deviceCfg).forEach(function (accessory, index, arr) {
                         myAccessories.push(accessory);
                     });
                 } else {
